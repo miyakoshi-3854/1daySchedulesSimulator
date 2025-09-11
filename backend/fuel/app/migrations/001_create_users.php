5c53650c8453a4d1f4257887e1cf2d64c872a7e5
miyakoshi-3854 <208133564+miyakoshi-3854@users.noreply.github.com>
2025-09-11 14:46:29 +0900

    ユーザーテーブルのマイグレーションファイルのインデントを4マスから2マスに整えました。

--- backend/fuel/app/migrations/001_create_users.php
+++ backend/fuel/app/migrations/001_create_users.php
@@ -4,22 +4,22 @@ namespace Fuel\Migrations;
 
 class Create_users
 {
-    // マイグレーションを実行する際に呼び出され`users`テーブルを作成するmethod
-    public function up()
-    {
-        \DBUtil::create_table('users', array(
-            'id' => array('constraint' => 11, 'type' => 'int', 'auto_increment' => true, 'unsigned' => true),
-            'name' => array('constraint' => 50, 'type' => 'varchar'),
-            'email' => array('constraint' => 255, 'type' => 'varchar', 'null' => true),
-            'password' => array('constraint' => 255, 'type' => 'varchar'),
-            'created_at' => array('type' => 'datetime', 'default' => \DB::expr('CURRENT_TIMESTAMP')),
-            'updated_at' => array('type' => 'datetime', 'default' => \DB::expr('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')),
-        ), array('id'));
-    }
+  // マイグレーションを実行する際に呼び出され`users`テーブルを作成するmethod
+  public function up()
+  {
+    \DBUtil::create_table('users', array(
+      'id' => array('constraint' => 11, 'type' => 'int', 'auto_increment' => true, 'unsigned' => true),
+      'name' => array('constraint' => 50, 'type' => 'varchar'),
+      'email' => array('constraint' => 255, 'type' => 'varchar', 'null' => true),
+      'password' => array('constraint' => 255, 'type' => 'varchar'),
+      'created_at' => array('type' => 'datetime', 'default' => \DB::expr('CURRENT_TIMESTAMP')),
+      'updated_at' => array('type' => 'datetime', 'default' => \DB::expr('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')),
+    ), array('id'));
+  }
 
-    // マイグレーションをロールバックする際に呼び出され`users`テーブルを削除するmethod
-    public function down()
-    {
-        \DBUtil::drop_table('users');
-    }
+  // マイグレーションをロールバックする際に呼び出され`users`テーブルを削除するmethod
+  public function down()
+  {
+    \DBUtil::drop_table('users');
+  }
 }
