1119bb529b0226bfef5234469573eae2a700a9af
miyakoshi-3854 <208133564+miyakoshi-3854@users.noreply.github.com>
2025-09-13 10:30:51 +0900

    スケジュールモデルにカテゴリーモデルとのリレーションシップを追加しました。

--- backend/fuel/app/classes/model/schedule.php
+++ backend/fuel/app/classes/model/schedule.php
@@ -18,15 +18,25 @@ class Model_Schedule extends \Orm\Model
 		'updated_at',
 	);
 
-	// Model_User(親)に属するModel_Schedule(子)のリレーションシップ
+
 	protected static $_belongs_to = array(
+		// Model_User(親)に属するModel_Schedule(子)の多対一のリレーションシップ
     'user' => array(
 			'key_from' => 'user_id',
 			'model_to' => 'Model_User',
 			'key_to' => 'id',
 			'cascade_save' => true,
 			'cascade_delete' => false,
-    )
+		),
+
+		// Model_category(親)に属するModel_Schedule(子)の多対一のリレーションシップ
+		'category' => array(
+			'key_from' => 'category_id',
+			'model_to' => 'Model_Category',
+			'key_to' => 'id',
+			'cascade_save' => true,
+			'cascade_delete' => false,
+    ),
 	);
 
 	// 作成日時と更新日時のオブザーバー
