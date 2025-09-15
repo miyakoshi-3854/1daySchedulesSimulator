d150b6820872ce59cd64aaff70d72dbafb72aefb
miyakoshi-3854 <208133564+miyakoshi-3854@users.noreply.github.com>
2025-09-15 23:10:27 +0900

    `Model_Schedule`のプロパティを詳細に定義し、バリデーションルールとオブザーバーを追加しました。

--- backend/fuel/app/classes/model/schedule.php
+++ backend/fuel/app/classes/model/schedule.php
@@ -11,17 +11,84 @@ class Model_Schedule extends \Orm\Model
 	 * ここで定義されたプロパティが、テーブルの各カラムに対応します。
 	 */
 	protected static $_properties = array(
-		'id',
-		'user_id',
-		'category_id',
-		'title',
-		'date',
-		'start_time',
-		'end_time',
-		'color',
-		'note',
-		'created_at',
-		'updated_at',
+		'id' => array(
+			'data_type' => 'int',
+			'null' => false,
+			'auto_increment' => true,
+			'primary_key' => true,
+			'label' => 'ID',
+		),
+		'user_id' => array(
+			'data_type' => 'int',
+			'null' => false,
+			'label' => 'ユーザーID',
+			'validation' => array(
+				'required',
+			),
+		),
+		'category_id' => array(
+			'data_type' => 'int',
+			'null' => true,
+			'label' => 'カテゴリーID',
+		),
+		'title' => array(
+			'data_type' => 'varchar',
+			'null' => false,
+			'label' => '予定名',
+			'validation' => array(
+				'max_length' => array(50),
+			),
+		),
+		'date' => array(
+			'data_type' => 'date',
+			'null' => false,
+			'label' => '日付',
+			'validation' => array(
+				'valid_date',
+			),
+		),
+		'start_time' => array(
+			'data_type' => 'time',
+			'null' => false,
+			'label' => '開始時間',
+			'validation' => array(
+				'required',
+				'match_pattern' => array('/^(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d$/'),
+			),
+		),
+		'end_time' => array(
+			'data_type' => 'time',
+			'null' => false,
+			'label' => '終了時間',
+			'validation' => array(
+				'required',
+				'match_pattern' => array('/^(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d$/'),
+			),
+		),
+		'color' => array(
+			'data_type' => 'varchar',
+			'null' => true,
+			'label' => '色',
+			'validation' => array(
+				'exact_length' => array(7),
+				'match_pattern' => array('/^#[0-9a-fA-F]{6}$/'),
+			),
+		),
+		'note' => array(
+			'data_type' => 'text',
+			'null' => true,
+			'label' => '備考',
+		),
+		'created_at' => array(
+			'data_type' => 'datetime',
+			'null' => false,
+			'label' => '作成日時',
+		),
+		'updated_at' => array(
+			'data_type' => 'datetime',
+			'null' => false,
+			'label' => '更新日時',
+		),
 	);
 
 	/**
@@ -56,6 +123,10 @@ class Model_Schedule extends \Orm\Model
 	 * データベース操作の前後に自動で実行される処理（**オブザーバー**）を定義します。
 	 */
 	protected static $_observers = array(
+		// `before_save` イベント時にバリデーション（入力値検証）を行うオブザーバー。
+		'Orm\Observer_Validation' => array(
+			'events' => array('before_save'), 
+		),
 		// 新規レコード作成時、`created_at`カラムにタイムスタンプを自動設定するオブザーバー。
 		'Orm\Observer_CreatedAt' => array(
 			'events' => array('before_insert'),
