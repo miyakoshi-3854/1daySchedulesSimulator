0e638bf7bc738f3ef9dac8e3d55b6be78e96d666
miyakoshi-3854 <208133564+miyakoshi-3854@users.noreply.github.com>
2025-09-22 16:23:47 +0900

    wip

--- backend/fuel/app/config/auth.php
+++ backend/fuel/app/config/auth.php
@@ -1,15 +1,14 @@
 <?php
 
 return array(
-  // 使用する認証ドライバを指定
-  'driver' => array('Simpleauth'),
-  // パスワードハッシュ化に使用されるソルト
+  'driver' => array('Simpleauth'),        // ← 配列にするのが正式
   'salt' => 'my_fuel_php_app_salt',
-  // 各ドライバの詳細設定を定義
+  'verify_multiple_logins' => false,
+  'iterations' => 5000,
+
   'drivers' => array(
     'Simpleauth' => array(
       'login_hash_salt' => 'my_fuel_php_app_login_hash_salt',
-      'iterations' => 5000,
       'table' => 'users',
       'model' => 'Model_User',
       'username_property' => 'name',
@@ -17,11 +16,5 @@ return array(
       'email_property'    => 'email',
       'login_after_registration' => true,
     ),
-  ),
-  // ログイン状態を保持する
-  'remember_me' => array(
-    'enabled' => true,
-    'cookie_name' => 'remember_me',
-    'expiration' => 604800, // 7日
   ),
 );
---
+++ backend/fuel/app/config/simpleauth.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+  'table' => 'users',
+  'table_columns' => array('*'),
+  'model' => 'Model_User',
+  'multiple_logins' => false,
+  'login_after_registration' => true,
+);
