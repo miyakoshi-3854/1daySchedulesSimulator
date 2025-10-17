ac69d3602b879de64e6714aa9f9740ef866bf16f
miyakoshi-3854 <208133564+miyakoshi-3854@users.noreply.github.com>
2025-10-17 15:15:47 +0900

    fix: Calendarコンポーネントを表示するようにコメントアウトを外した。

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import { AuthContextProvider } from '../contexts/AuthContext';
 import { DateContextProvider } from '../contexts/DateContext';
 import { ScheduleContextProvider } from '../contexts/ScheduleContext';
 import { Header } from '../components/Header';
-// import Calendar from '../components/Calendar';
+import { Calendar } from '../components/Calendar';
 // import TimeGraph from '../components/TimeGraph';
 // import ScheduleList from '../components/ScheduleList';
 // import ScheduleForm from '../components/ScheduleForm';
@@ -13,8 +13,8 @@ export default function DashBoard() {
       <DateContextProvider>
         <ScheduleContextProvider>
           <Header />
-          {/* <Calendar />
-          <TimeGraph />
+          <Calendar />
+          {/* <TimeGraph />
           <ScheduleList />
           <ScheduleForm /> */}
         </ScheduleContextProvider>
